bca144412682a39dede60909a15c3869ca1cb7ec
K-30\TEACHER <66kbz@soscandia.org>
2024-12-04 16:58:38 +0700

    Добавил классы животных

---
+++ FermMad/IAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermMad
+{
+    public interface IAnimal
+    {
+        bool Health { get; }
+        int Eat { get; }
+        int PriceBuy { get; }
+        int PriceSell { get; }
+    }
+}
