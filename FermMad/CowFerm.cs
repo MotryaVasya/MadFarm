bca180abc4333677abb321658527ca9363da87d9
DESKTOP-DESRL0V\Student <hackydoom@yandex.ru>
2024-12-04 16:58:42 +0700

    create interface + classes

---
+++ FermMad/CowFerm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermMad
+{
+    public class CowwFerm : IFerm
+    {
+        private List<IAnimal> _animals;
+        private int _max_count_animals;
+        public int max_count_animals => _max_count_animals;
+
+        public List<IAnimal> animals { get => _animals; }
+    }
+}
---
+++ FermMad/IFerm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermMad
+{
+    internal interface IFerm
+    {
+        int max_count_animals { get; }
+        List<IAnimal> animals { get; }
+    }
+
+}
