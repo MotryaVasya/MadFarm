19f55623bb64e12205efd39ba6f725b463dcf607
Vasya Motrya <motravasa5@gmail.com>
2024-12-06 16:05:07 +0700

    добавил ButtonCommands

---
+++ FermMad/ButtonCommands.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FermMad
+{
+    public class ButtonCommands : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action _action;
+        public ButtonCommands(Action action)
+        {
+            _action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+        public ButtonCommands BuyFerm()
+        {
+            return new ButtonCommands(() => 
+            {
+                // сделать покупку 
+            });
+        }
+        
+
+    }
+}
