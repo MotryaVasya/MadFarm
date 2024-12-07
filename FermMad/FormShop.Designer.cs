7895d5b22edef06c3e76d24c22c94956c6661ff8
DESKTOP-4O0FQIM\User <sonyacat135@mail.ru>
2024-12-07 20:16:24 +0700

    добавила форму магазина и лист боксы

---
+++ FermMad/FormShop.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FermMad
+{
+    partial class FormShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(30, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(279, 388);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(657, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 61);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(639, 342);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 85);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FormShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "FormShop";
+            this.Text = "FormShop";
+            this.Load += new System.EventHandler(this.FormShop_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
---
+++ FermMad/FormShop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FermMad
+{
+    public partial class FormShop : Form
+    {
+        new ListBox listBox2 = new ListBox();
+        new TextBox textBox1 = new TextBox();
+        public FormShop()
+        {
+            InitializeComponent();
+            listBox1.Items.Add[0];
+            listBox1.Items.Add[1];
+            listBox1.Items.Add[2];
+           
+        }
+        private void FormShop_Load(object sender, EventArgs e)
+        {
+            ShopElements(textBox1.Text);
+            ShopElements(listBox2.Text);
+        }
+
+        private void ShopElements(string Text)
+        {
+            switch(Text)
+            {
+                case "Постройки":
+                    listBox2.Items.Add("Ферма коров");
+                    listBox2.Items.Add("Ферма свиней");
+                    listBox2.Items.Add("Ферма куриц");
+                    listBox2.Items.Clear();
+                    break;
+                case "Животные":
+                    listBox2.Items.Add("Курицы");
+                    listBox2.Items.Add("Свиньи");
+                    listBox2.Items.Add("Коровы");
+                    listBox2.Items.Clear();
+                    break;
+                case "Корм":
+                    listBox2.Items.Add("Корм");
+                    listBox2.Items.Clear();
+                    break;
+                case "ПокупкаФермы":
+                    ViewModel.BuyFerm.GetType(BuyElementsShop.BuyFerm(listBox2.SelectedIndex));
+                    break;
+                case "ПокупкаЖивотных":
+                    ViewModel._ferms.Add(BuyElementsShop.BuyAnimal(listBox2.SelectedIndex));
+                    break;
+                case "ПокупкаКорма":
+                    ViewModel.BuyCorm.ToString(BuyElementsShop.BuyCorm(listBox2.SelectedIndex));
+                    break;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
